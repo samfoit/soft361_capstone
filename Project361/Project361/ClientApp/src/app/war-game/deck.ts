0a01bd60a8ff15806ccb0f6733b13cd8e82e05c6
atChris2 <89475363+atChris2@users.noreply.github.com>
2023-05-12 18:46:06 -0500

    refactor war code

---
+++ Project361/Project361/ClientApp/src/app/war-game/deck.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../services/shared.service';
+import { Card } from '../models/card.model';
+import { Player } from '../models/player-dto.model';
+
+
+export class deck {
+
+  shuffle(players, deck) {
+    for (let i = 0; i < players.length; i++) {
+      let j = Math.floor(Math.random() * (deck.length));
+
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+  }
+
+  flipCards(players, deck) {
+    for (let i = 0; i < players.length; i++) {
+      deck[i].visible = true;
+    }
+    return deck;
+  }
+
+  dealRound(players, deck) {
+    let max = 0;
+
+    for (let i = 0; i < players.length; i++) {
+      if (deck[i].rank == 1) {
+        max = 14;
+      }
+      if (deck[i].rank > max) {
+        max = deck[i].rank;
+      }
+    }
+    
+    for (let i = 0; i < players.length; i++) {
+      if (deck[i].rank == max || (max == 14 && deck[i].rank == 1)) {
+        players[i].score += 1;
+        console.log(players[i]);
+
+        if (players[i].score >= 10) {
+          alert(players[i].name + " wins!");
+        }
+      }
+    }
+    return players;
+  }
+}
--- Project361/Project361/ClientApp/src/app/war-game/war-game.component.ts
+++ Project361/Project361/ClientApp/src/app/war-game/war-game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../services/shared.service';
 import { Card } from '../models/card.model';
 import { Player } from '../models/player-dto.model';
+import * as deck from './deck';
 
 @Component({
   selector: 'app-war-game',
@@ -21,9 +22,20 @@ export class WarGameComponent implements OnInit {
     this.startGame();
   }
   onPlayButtonClicked() {
-    this.shuffle();
-    this.flipCards();
-    this.dealRound();
+    let d = new deck.deck();
+    this.deck = d.shuffle(this.players, this.deck);
+    this.showingCards = true;
+    this.deck = d.flipCards(this.players, this.deck);
+    setTimeout(() => {
+      for (let i = 0; i < this.players.length; i++) {
+        this.deck[i].visible = false;
+      }
+    }, 5000);
+    this.showingCards = false;
+    this.players = d.dealRound(this.players, this.deck);
+    for (let i = 0; i < this.players.length; i++) {
+      this.service.updatePlayer(this.players[i] as Player).subscribe(response => { console.log(response); });
+    }
   }
 
   startGame() {
@@ -44,53 +56,5 @@ export class WarGameComponent implements OnInit {
     this.service.getDeck().subscribe(response => {
       this.deck = response;
     });
-  }
-
-  shuffle() {
-    for (let i = 0; i < this.players.length; i++) {
-      let j = Math.floor(Math.random() * (this.deck.length));
-
-      [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
-    }
-  }
-
-  flipCards() {
-    this.showingCards = true;
-    for (let i = 0; i < this.players.length; i++) {
-      this.deck[i].visible = true;
-    }
-
-    setTimeout(() => {
-      for (let i = 0; i < this.players.length; i++) {
-        this.deck[i].visible = false;
-      }
-      this.showingCards = false;
-    }, 5000);
-  }
-
-  dealRound() {
-    let max = 0;
-
-    for (let i = 0; i < this.players.length; i++) {
-      if (this.deck[i].rank == 1) {
-        max = 14;
-      }
-      if (this.deck[i].rank > max) {
-        max = this.deck[i].rank;
-      }
-    }
-
-    for (let i = 0; i < this.players.length; i++) {
-      if (this.deck[i].rank == max || (max == 14 && this.deck[i].rank == 1)) {
-        this.players[i].score += 1;
-        console.log(this.players[i]);
-        this.service.updatePlayer(this.players[i] as Player).subscribe(response => { console.log(response); });
-        
-
-        if (this.players[i].score >= 10) {
-          alert(this.players[i].name + " wins!");
-        }
-      }
-    }
   }
 }
