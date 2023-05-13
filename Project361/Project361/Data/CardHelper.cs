eaa34511d3dca7775bfed9f1db44e76b64af9385
pnikam2 <89424739+pranav29003@users.noreply.github.com>
2023-05-13 16:52:55 -0500

    abstracted the method in CardsContext.cs
    added a new CardHelper.cs class to abstract the method in CardsContext.cs

---
+++ Project361/Project361/Data/CardHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Project361.Data
+{
+    public class CardHelper
+    {
+        public static int GetCardRank(int id)
+        {
+            int result = id % 13;
+
+            if (result == 0)
+            {
+                return 13;
+            }
+
+            return result;
+        }
+
+        public static string GetCardSuite(int id)
+        {
+            string result = "";
+
+            if (id <= 13)
+            {
+                result = "Spades";
+            }
+            else if (id <= 26)
+            {
+                result = "Hearts";
+            }
+            else if (id <= 39)
+            {
+                result = "Clubs";
+            }
+            else
+            {
+                result = "Diamonds";
+            }
+
+            return result;
+        }
+    }
+}
+
+
--- Project361/Project361/Data/CardsContext.cs
+++ Project361/Project361/Data/CardsContext.cs
@@ -7,13 +7,12 @@ namespace Project361.Data;
 
 public class CardsContext : DbContext
 {
-	public CardsContext(DbContextOptions<CardsContext> options) : base(options) { }
+    public CardsContext(DbContextOptions<CardsContext> options) : base(options) { }
 
-	public DbSet<Card> Cards { get; set; }
+    public DbSet<Card> Cards { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Call the base version of this method (in DbContext) as well, else we sometimes get an error later on.
         base.OnModelCreating(modelBuilder);
 
         var cardsToSeed = new Card[52];
@@ -23,47 +22,12 @@ public class CardsContext : DbContext
             cardsToSeed[i - 1] = new()
             {
                 Id = i,
-                Rank = GetCardRank(i),
-                Suite = GetCardSuite(i),
+                Rank = CardHelper.GetCardRank(i), 
+                Suite = CardHelper.GetCardSuite(i),
                 Visible = false
             };
         }
 
         modelBuilder.Entity<Card>().HasData(cardsToSeed);
     }
-
-    // abstract these two methods into a static C# class
-    private int GetCardRank(int id)
-    {
-        int result = id % 13;
-
-        if (result == 0)
-        {
-            return 13;
-        }
-
-        return result;
-    }
-
-    private string GetCardSuite(int id)
-    {
-        string result = "";
-
-        if (id <= 13)
-        {
-            result = "Spades";
-        } else if (id <= 26)
-        {
-            result = "Hearts";
-        } else if (id <= 39)
-        {
-            result = "Clubs";
-        } else
-        {
-            result = "Diamonds";
-        }
-
-        return result;
-    }
 }
-
